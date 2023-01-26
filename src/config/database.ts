4a1a97c4430ca96f36823f4741a98494782219c1
Emerson <emersonkilon@gmail.com>
2023-01-26 13:26:19 -0300

    atividade_finalizada

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,20 @@
-import pg from "pg";
+ import pg from "pg";
+ import {PrismaClient} from "@prisma/client"
 import dotenv from "dotenv";
 dotenv.config();
 
-const { Pool } = pg;
+/* const { Pool } = pg;
 const configDatabase = {
   connectionString: process.env.DATABASE_URL
 };
 
 const db = new Pool(configDatabase);
-export default db;
+ */
+
+
+const database = new PrismaClient({
+  log: ["error", "info", "query", "warn"]
+})
+
+
+export {database}
